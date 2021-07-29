1bd6e360981d9fef732387cfa055e45a08587ec6
fffirer <0sdf0@sina.cn>
2021-07-30 00:47:15 +0800

    UPDATE: 更新vite构建时得基本路径

--- LittleBlog.Admin/vite.config.ts
+++ LittleBlog.Admin/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { loadEnv } from 'vite'
+import { def } from '@vue/shared'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ mode }) => {
+    return defineConfig({
+        plugins: [vue()],
+        base: loadEnv(mode, process.cwd()).VITE_APP_NAME
+    })
+}
