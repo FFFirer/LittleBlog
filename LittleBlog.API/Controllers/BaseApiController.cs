b7173a64fce99ba0cd0d93f487e9c21d6cd15e5f
fffirer <0sdf0@sina.cn>
2022-03-29 23:08:50 +0800

    [feature] 拆分接口为独立项目

---
+++ LittleBlog.API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LittleBlog.API.Controllers
+{
+    /// <summary>
+    /// 控制器基类
+    /// </summary>
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
